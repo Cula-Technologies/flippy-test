a98365c1a42f9ac7e7c3b476c6d451a836b36a8d
Jochen Weber <jochen.weber80@gmail.com>
2025-01-30 23:06:19 +0100

    select device type by node env

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 import { Display, createServer } from './index.js'
 import { layout, devices, nullDevices, options } from './config/config.js'
 
-Display.configure(layout, devices, options)
-//Display.configure(layout, nullDevices, options)
-const app = createServer()const {app, server} = createServer()
+
+if (process.env.NODE_ENV === 'production') {
+    Display.configure(layout, devices, options)
+} else {
+    Display.configure(layout, nullDevices, options)
+}
+const {app, server} = createServer()
