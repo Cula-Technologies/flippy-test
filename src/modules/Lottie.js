dd6c3dbc3e0f070edb42db162e1fafa319070f43
jonasnoack <jonasnoackgruen@gmail.com>
2025-01-31 12:47:27 +0100

    remove lottie console logs

--- src/modules/Lottie.js
+++ src/modules/Lottie.js
@@ -25,35 +25,21 @@ export default class Lottie extends Module {
   async add(obj) {
     const loadingConfig = {canvas: this.canvas, ...this.config, src: obj.url };
     this.dotLottie = new DotLottie(loadingConfig);
-    console.log('loading lottie', loadingConfig)
     this.dotLottie.load(loadingConfig);
 
     
     this.dotLottie.addEventListener('ready', (event) => {
-      console.log('ready', event);
       this.dotLottie.play();
     })
     this.dotLottie.addEventListener('load', (event) => {
-      console.log('load', event);
       this.dotLottie.play();
     })
-    // this.dotLottie.addEventListener('frame', (event) => {
-    //   console.log('frame', event);
-    // })
-    // this.dotLottie.addEventListener('render', (event) => {
-    //   console.log('render', event);
-    // }) 
-
     this.dotLottie.addEventListener('loadError', (event) => {
       console.log('error', event);
     })
   }
 
   render() {
-    // find out if image has any values
-    const imageData = this.canvas.getContext('2d').getImageData(0, 0, 100, 100).data;
-    const notNullValues = imageData.filter((value) => value !== 0);
-    // console.log('render', notNullValues.length);
   }
 
   destroy() {
